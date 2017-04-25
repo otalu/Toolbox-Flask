d5f72b930b86777a72d615bace2e1d8745004549
otalu <onurtalu@gmail.com>
2017-04-24 22:26:11 -0400

    Added the necessary files for Heroku to run

--- database.py
+++ database.py
@@ -6,4 +6,4 @@ Author: Onur Talu
 """
 
 user_db = [['Inigo', 'Montoya', '10'], ['Onur', 'Talu', '5'],
-           ['Evan', 'New-Schmidt', '19']]
+           ['Evan', 'New-Schmidt', '19'], ['Colvin', 'Chapman', '19']]
